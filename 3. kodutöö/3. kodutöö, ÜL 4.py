ebddf3d05fdb959d9077dcda8d066e4c2572ade0
kristjan0 <1533424+kristjan0@users.noreply.github.com>
2024-11-19 20:11:08 +0200

    yl4 parandused

--- 3. kodutöö/3. kodutöö, ÜL 4.py
+++ 3. kodutöö/3. kodutöö, ÜL 4.py
@@ -16,19 +16,19 @@ def tersearch(otsitav, massiiv, vasak=0, parem=None):
         kolmandik2 = parem - (parem - vasak) // 3
 
 
-    if massiiv[kolmandik] == otsitav: #Kui otsitav element on esimeses kolmandikus, tagastame selle indeksi
-        return kolmandik
-    if massiiv[kolmandik2] == otsitav: #Kui otsitav element on kolmandas kolmandikus, tagastame selle indeksi
-        return kolmandik2
+        if massiiv[kolmandik] == otsitav: #Kui otsitav element on esimese kolmandiku jagamispunkt, tagastame selle indeksi
+            return kolmandik
+        if massiiv[kolmandik2] == otsitav: #Kui otsitav element on kolmanda kolmandiku jagamispunkt, tagastame selle indeksi
+            return kolmandik2
 
 
-    if otsitav < massiiv[kolmandik]: #Kui otsitav element on esimese kolmandiku ja jagamispunkti vahel, siis jätkame esimese kolmandikuga
-        return tersearch(vasak, kolmandik-1, otsitav, massiiv)
-    elif otsitav > massiiv[kolmandik2]: #Kui otsitav element on kolmanda kolmandiku ja jagamispunkti vahel, siis jätkame kolmanda kolmandikuga
-         return tersearch(kolmandik2+1, parem, otsitav, massiiv)
+        if otsitav < massiiv[kolmandik]: #Kui otsitav element on esimese kolmandiku ja jagamispunkti vahel, siis jätkame esimese kolmandikuga
+            return tersearch(vasak, kolmandik-1, otsitav, massiiv)
+        elif otsitav > massiiv[kolmandik2]: #Kui otsitav element on kolmanda kolmandiku ja jagamispunkti vahel, siis jätkame kolmanda kolmandikuga
+            return tersearch(kolmandik2+1, parem, otsitav, massiiv)
 
-    if otsitav > massiiv[kolmandik] and otsitav < massiiv[kolmandik2]: #Kui otsitav element on jagamispunktide vahel, siis jätkame keskmise kolmandikuga
-        return tersearch(kolmandik+1, kolmandik2-1, otsitav, massiiv)
+        if otsitav > massiiv[kolmandik] and otsitav < massiiv[kolmandik2]: #Kui otsitav element on jagamispunktide vahel, siis jätkame keskmise kolmandikuga
+            return tersearch(kolmandik+1, kolmandik2-1, otsitav, massiiv)
     
     #Kui elementi ei leita, tagastame -1
     return -1
