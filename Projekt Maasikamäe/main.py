681882ff1e9fd5f86565ebf991a6fa442eef199f
Marcus Maasikamäe <marcusn3001@hotmail.com>
2024-12-10 15:34:05 +0200

    hehee, peaaaegu valmis, nüüd sprteerima

--- Projekt Maasikamäe/main.py
+++ Projekt Maasikamäe/main.py
@@ -30,13 +30,11 @@ def Skoor(Mängijad, Atribuudid):
                 atribuudid_listis_üksikult.append(element)
         
         atribuudid_listis.append(atribuudid_listis_üksikult)
-        print(atribuudid_listis)
-
 
 
     mängija = []
     i = 0
-
+    lõpp = []
     for line in players:
         if i < 53:  #Kuna ühe mängija kohta on alati 52 rida ja 2 tühja rida, siis saan selle
                     #defineerimsega võtta ühe mängija atribuudid ja need listi salvestada
@@ -51,31 +49,53 @@ def Skoor(Mängijad, Atribuudid):
             i = i + 1
 
 
-
-
-
-
-
-
-
-
-
-
-
             if i == 53:
+                mängija_tulemuse_list = []
                 mängija = mängija[:len(mängija)-2]
                 mängija_andmed = mängija[:4]
                 mängija_atribuudid = mängija[4:]
-
+                s = {}
                 
+                for element in atribuudid_listis:
+                    tulemus = []
                     
+                    for pikkus in range(len(mängija_atribuudid)):
+                        
+                        a = (mängija_atribuudid[pikkus] * element[pikkus])
+                        tulemus.append(a)
+                        if pikkus == 46:
+                            positsioon = element[-1]
+                            tulemus = sum(tulemus)
+                            skoorid = positsioon, tulemus
+                            s.update({skoorid})
+                            if len(s) == 11:
+                                keymax = max(s, key = lambda x: s[x])
+                                parim_pos = [keymax, mängija_andmed[0], s.get(keymax) ,mängija_andmed[2]]
+                                lõpp.append(parim_pos)
+                                #print(lõpp)
+                #print(lõpp)
+                                
+
+                                #mängija_tulemuse_list.append()
+                                #print(mängija_tulemuse_list)
+                #print(mängija_tulemuse_list)            
+
                 #for atribuut in mängija_atribuudid:
 
 
 
                 mängija = []
                 i = 0
-
+    sorteeritud = sorted(lõpp, key=lambda x: x[0])
     
-
+    def sorteeri(lst):
+        x = [item[0] for item in lst]
+        x = list(set(x))
+        return x
+        
+        #lõpp
+    print(sorteeri(lõpp))
+    #for lement in lõpp:
+        
+         
 Skoor('CasaPiaAndmestrukt.txt', 'Attributes.txt')
